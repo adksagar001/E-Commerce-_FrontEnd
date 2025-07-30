640a4520b6879f7a8d372ffed1b7f58f352dfa47
Sagar Adhikari <idatenjumpzz@gmail.com>
2025-07-30 12:46:31 +0545

    products pages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,76 @@
 import React from "react";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-
+import AdminProductsList from "./pages/Admin/AdminProductsList";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   useLocation,
 } from "react-router-dom";
+
+import SideBar from "./pages/Admin/AdminSideBar";
 import ProductsList from "./pages/ProductsList";
+import NotFound from "./pages/NotFound";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Navbar from "./components/Navbar";
 import Mainbody from "./components/MainBody";
 import Footer from "./components/Footer";
-import ThemeToggler from "./utils/ThemeToggler";
 import ForgotPassword from "./pages/ForgotPassword"; // Import ForgotPassword page
 import AdminDashboard from "./pages/Admin/AdminDashboard";
 
-// Wrapper component to conditionally render Footer
+//----------------------Place footer only on Customer Side-----------------//
 const AppWithFooter = () => {
   const location = useLocation();
 
-  // Hide footer on login and register pages
   if (
     location.pathname === "/login" ||
     location.pathname === "/register" ||
-    location.pathname === "/forgot-password"
+    location.pathname === "/forgot-password" ||
+    location.pathname === "/admindashboard" ||
+    location.pathname === "/admin/products"
   ) {
-    return null; // Don't render Footer
+    return null;
+  } else if (
+    location.pathname.startsWith("/apps/") ||
+    location.pathname.startsWith("/admin/")
+  ) {
+    return null;
   }
 
-  return <Footer />; // Render Footer for all other pages
+  return <Footer />;
+};
+//----------------------Place NavBar on Top For Customers and SideBar For Admin-----------------//
+const AppwithNavBarOnTop = () => {
+  const location = useLocation();
+  if (
+    location.pathname === "/admindashboard" ||
+    location.pathname === "/admin/products"
+  ) {
+    return <SideBar />;
+
+  } else if (
+    location.pathname.startsWith("/apps/") ||
+    location.pathname.startsWith("/admin/")
+  ) {
+    return null;
+  } else return <Navbar />;
 };
 
 function App() {
   return (
     <Router>
-      {/* <ThemeToggler /> */}
-      <Navbar />
+      <AppwithNavBarOnTop />
       <Routes>
         <Route path="/" element={<Mainbody />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />{" "}
         <Route path="/admindashboard" element={<AdminDashboard />} />
+        <Route path="/admin/products" element={<AdminProductsList />} />
+        <Route path="*" element={<NotFound />} /> 
       </Routes>
-      <AppWithFooter /> {/* Conditionally render Footer */}
+      <AppWithFooter />
     </Router>
   );
 }
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,29 @@
+// src/pages/NotFound.js
+import React from "react";
+
+const NotFound = () => {
+//   return (
+//     <div className="text-center mt-5">
+//       <h1>404 - Page Not Found</h1>
+//       <p>The page you’re looking for doesn’t exist.</p>
+//     </div>
+    //   );
+    return (<main className="main" id="top">
+        <div className="px-3">
+            <div className="row min-vh-100 flex-center p-5">
+                <div className="col-12 col-xl-10 col-xxl-8">
+                    <div className="row justify-content-center align-items-center g-5">
+                        <div className="col-12 col-lg-6 text-center order-lg-1"><img className="img-fluid w-lg-100 d-dark-none" src="../../public/assets/spot-illustrations/404-illustration.png" alt="" width="400" /><img className="img-fluid w-md-50 w-lg-100 d-light-none" src="../../assets/img/spot-illustrations/dark_404-illustration.png" alt="" width="540" /></div>
+                        <div className="col-12 col-lg-6 text-center text-lg-start"><img className="img-fluid mb-6 w-50 w-lg-75 d-dark-none" src="../../assets/img/spot-illustrations/404.png" alt="" /><img className="img-fluid mb-6 w-50 w-lg-75 d-light-none" src="../../assets/img/spot-illustrations/dark_404.png" alt="" />
+                            <h2 className="text-body-secondary fw-bolder mb-3">Page Missing!</h2>
+                            <p className="text-body mb-5">But no worries! Our ostrich is looking everywhere <br className="d-none d-sm-block" />while you wait safely. </p><a className="btn btn-lg btn-primary" href="../../index.html">Go Home</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </main>);
+};
+
+export default NotFound;
